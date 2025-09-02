e595c4f48b285b60d21adcd3b78a8649211a7048
Adrian BQ <jan_uva430@outlook.com>
2025-09-02 11:26:09 -0700

    Add files via upload

---
+++ jobs/jobpython.py
@@ -0,0 +1,11 @@
+numeros = [1, 2, 3, 4, 5]
+print("Lista inicial:", numeros)
+
+print("Primer elemento:", numeros[0])
+print("Último elemento:", numeros[-1])
+
+numeros.append(6)
+print("Después de agregar 6:", numeros)
+
+for n in numeros:
+    print("Elemento:", n)
