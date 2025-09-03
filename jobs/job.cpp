228ee794e988c76ae3e39c7866117b0dba2991c2
Adrian BQ <jan_uva430@outlook.com>
2025-09-03 11:14:23 -0700

    Add files via upload

---
+++ jobs/job.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numeros[3] = {5, 10, 15};
+
+    cout << "Elementos del arreglo:" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << numeros[i] << endl;
+    }
+
+    return 0;
+}
+
